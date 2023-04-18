ce949490d1c9537a7345ad6470f9f05d5911048f
Evan McPheron <evaninja2@gmail.com>
2023-04-17 22:37:15 -0500

    added todo!() methods for video rendering to scene

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub use error_stack::{Context, IntoReport, Report, Result, ResultExt};
 pub use error_stack_derive::ErrorStack;
 pub use imageproc::point::Point;
-//ffmpeg -reinit_filter 0 -f concat -safe 0 -i "ffmpeg.Txt" -vf "scale=1280:720:force_original_aspect_ratio=decrease:eval=frame,pad=1280:720:-1:-1:color=black:eval=frame,settb=AVTB,format=yuv420p" -r 15 output.mp4
+
 
 pub mod frame_dictionary;
 pub mod renderable;
--- src/scene.rs
+++ src/scene.rs
@@ -10,6 +10,12 @@ use std::time::Duration;
 
 use super::renderable::*;
 
+#[derive(ErrorStack, Debug)]
+#[error_message("An error occured while rendering a scene")]
+pub enum SceneRenderingError {
+    Null
+}
+
 pub struct Scene {
     children: Vec<Arc<RwLock<Renderable>>>,
     resolution: Point<usize>,
@@ -21,6 +27,54 @@ pub struct Scene {
 impl Scene {
     pub fn builder() -> SceneBuilder {
         SceneBuilder { children: Some(vec![]), resolution: Some(Point::new(1280,720)), fps: Some(30), length: None, output_filename: Some(PathBuf::from("output.mp4")) }
+    }
+    pub fn get_children(&self) -> &Vec<Arc<RwLock<Renderable>>> {
+        &self.children
+    }
+    pub fn get_children_mut(&mut self) -> &mut Vec<Arc<RwLock<Renderable>>> {
+        &mut self.children
+    }
+    pub fn add_child_simple(&mut self, child: Renderable) -> Arc<RwLock<Renderable>> {
+        let s = Arc::new(RwLock::new(child));
+        self.children.push(s.clone());
+        s
+    }
+    pub fn add_child(&mut self, child: Arc<RwLock<Renderable>>) {
+        self.children.push(child);
+    }
+    fn render_frames() -> Result<(), SceneRenderingError> {
+        //delete frame directory
+        //create frame directory
+        //figure out frame count, with matching duration to send to behaviour and shader
+        //for each frame, run render frame
+        todo!()
+    }
+    fn render_frame() -> Result<(), SceneRenderingError> {
+        //create an empty rgba image buffer
+        //'recursively' iterate through all children of the scene, and their children, (run from top down)
+        //for each child, run their run their behaviour's process, then for every pixel, run their get_pixel (THIS CAN EASILY BE PARRELLELIZED) and overide the pixel on the main image buffer'
+        //write image buffer to file
+        todo!()
+    }
+    /*
+    fn post_process_frame() -> Result<(), SceneRenderingError> {
+        In future, potentially add an optional post-processing feild to scene, which takes an takes the final image buffer and is free to operate on it any way it wants.
+        That would be run in this fn, which would be run directly after the 'render_frame()' fn.
+    }
+     */
+    fn generate_frame_dictionary() -> Result<(), SceneRenderingError> {
+        //Create and save a frame dictionary
+        todo!()
+    }
+    fn compile_video() -> Result<(), SceneRenderingError> {
+        //Use this command (add formatting)
+        //ffmpeg -reinit_filter 0 -f concat -safe 0 -i "ffmpeg.Txt" -vf "scale=1280:720:force_original_aspect_ratio=decrease:eval=frame,pad=1280:720:-1:-1:color=black:eval=frame,settb=AVTB,format=yuv420p" -r 15 output.mp4
+        //print outputs to stdout, maybe in the future add formatting?
+        todo!()
+    }
+    pub fn render(&self) -> Result<(), SceneRenderingError> {
+        
+        todo!()
     }
 }
 
